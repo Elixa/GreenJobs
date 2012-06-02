f629cce401ad50e15bcc09c1063f663347d68fe5
Frangeris Peguero <frangeris.20@gmail.com>
2012-06-02 14:55:24 -0300

    Update

--- style/web/include/head.php
+++ style/web/include/head.php
@@ -22,7 +22,7 @@
 		
 		<!--GoogleMap
 	    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-	-->
+		-->
 		<script type="text/javascript">
 			jQuery(document).ready(function($) {
 				$("a[rel*=Jfancy]").Jfancy({
